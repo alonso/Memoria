cb1825bce01051762be39c24a07c43dedc9ff311
alonso <alogonza@ing.uchile.cl>
2011-01-28 15:13:42 -0300

    Primer commit

---
+++ clear.sh
@@ -0,0 +1,8 @@
+#/bin/sh
+rm memoria.aux
+rm memoria.fix
+rm memoria.lof
+rm memoria.log
+rm memoria.out
+rm memoria.pdf
+rm memoria.toc
---
+++ meic.sh
@@ -0,0 +1,3 @@
+#/bin/sh
+pdflatex memoria.tex
+cp memoria.pdf ../
